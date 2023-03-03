862ae53505a21cebdd901e2c890cae5d57c4c35c
Simon Emms <simon@simonemms.com>
2023-03-03 18:16:16 +0000

    feat: configure markdown-based posts

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,16 @@
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { globSync as glob } from 'glob';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  extensions: ['.svelte', ...mdsvexConfig.extensions],
+
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
 
   kit: {
     adapter: adapter({
@@ -15,6 +20,18 @@ const config = {
       precompress: false,
       strict: true,
     }),
+    prerender: {
+      entries: [
+        '*',
+        ...glob('./src/posts/*.{svelte.md,md,svx}').map((i) =>
+          i
+            .replace('src', '')
+            .replace(/\.svelte\.md/, '')
+            .replace(/\.md/, '')
+            .replace(/\.svx/, ''),
+        ),
+      ],
+    },
   },
 };
 
