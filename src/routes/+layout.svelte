8635d1cf7353475cf10c3ef70af718ebe5b24b3d
Simon Emms <simon@simonemms.com>
2023-03-05 18:20:52 +0000

    chore: add logo to header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,28 @@
   import '../app.scss';
 </script>
 
-<slot />
+<section class="container">
+  <nav class="navbar is-transparent" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">&nbsp;</a>
+    </div>
+  </nav>
+</section>
+
+<section class="container">
+  <slot />
+</section>
+
+<style lang="scss">
+  .navbar-item {
+    border: 1px solid red;
+    background: {
+      image: url(/img/logo.png);
+      position: center center;
+      repeat: no-repeat;
+      size: cover;
+    }
+    height: 100px;
+    width: 332px;
+  }
+</style>
