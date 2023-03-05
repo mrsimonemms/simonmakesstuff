a68a8b4f7d1f2d044a365a19ac98cb111306ea59
Simon Emms <simon@simonemms.com>
2023-03-05 14:43:57 +0000

    chore: configure bulma

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import preprocess from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-static';
@@ -10,7 +11,15 @@ const config = {
 
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
+  preprocess: [
+    vitePreprocess(),
+    mdsvex(mdsvexConfig),
+    preprocess({
+      scss: {
+        prependData: '@use "src/variables.scss" as *;',
+      },
+    }),
+  ],
 
   kit: {
     adapter: adapter({
