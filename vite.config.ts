461adc9b2ce748d41a9e4135b3463abb61775f35
Simon Emms <simon@simonemms.com>
2024-03-25 17:08:09 +0000

    feat: add bulma

--- vite.config.ts
+++ vite.config.ts
@@ -19,10 +19,12 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+
 	server: {
 		host: process.env.VITE_HOST,
 		port: Number(process.env.VITE_PORT ?? 5173),
 	},
+
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
 	},
